f214583b8ec540b6be72d30950b4fccb12d8faad
AureliaIzza <aurelia.izzanur.salsabila@gmail.com>
2025-05-15 13:57:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 st.title("🧸Aurelia app")
 st.write("welcome!💙")
-st.image("IMG-20250401-WA0031.jpeg", width=200)
+st.image("IMG_20250515_134118.jpg", width=200)
 st.write("\n")
 st.subheader("Aurel💗Heeseung")
 st.write("Lee Heeseung Punya Aurel")
-st.image("IMG_20250515_134118.jpg", width=200)
 
