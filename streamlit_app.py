9ada6dc3338b53bcc4c974f132a8e3cffd1d1661
AureliaIzza <aurelia.izzanur.salsabila@gmail.com>
2025-05-22 13:11:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,3 @@ st.write("\n")
 st.subheader("Aurel💗Heeseung")
 st.write("Lee Heeseung Punya Aurel")
 
-#with col1:
-st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
-angka st.number_input("Tulis sebuah Angka:", value=0, step=1)
-if (angka % 2) == 0:
-  st.write(f"{angka} adalah Bilangan Genap")
-else:
-  st.write(f"{angka} adalah Bilangan Ganjil")
- 
